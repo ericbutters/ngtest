14e618c33da302e1d205a4ff4594a3a4a828080f
Daniel <daniel@eric-X220tp.(none)>
2014-12-21 02:50:05 +0100

    Improve updating dates and times

--- app/scripts/controllers/ericCtrl.js
+++ app/scripts/controllers/ericCtrl.js
@@ -59,13 +59,57 @@ angular.module('ngtestApp')
 			$modalInstance.dismiss('cancel');
 		};
 		$scope.setActiveDate = function(d) {
-			$scope.acdate = d;
+			//$scope.acdate = d;
+			$scope.actimeorig = [];
+			$scope.actime = [];
+			$scope.actdateorig = d;
+			console.log("--> " + JSON.stringify(d));
+			$scope.acdate = angular.copy(d);
+			console.log("copied date: " + JSON.stringify($scope.acdate) + " .. " + JSON.stringify($scope.actdateorig));
+			$scope.$watch('acdate.date', function(newvalue,oldvalue) {
+				if(newvalue != oldvalue /*&& (newvalue.match(/^[0-2][0-3]:[0-5][0-9]$/))*/)
+					$scope.showApplyDate(true);
+				else
+					$scope.showApplyDate(false);
+			});	
 		}
 		$scope.deleteActiveDate = function(i) {
 			$scope.items.meeting.data.splice(i,1);
 		}
+		$scope.showApplyDate = function(show){
+			$scope.isCollapsedDate = !show;
+		}
+		$scope.applyDateChange = function() {
+			console.log("apply date change: " + JSON.stringify($scope.acdateorig) + " .. " + JSON.stringify($scope.acdate));
+			$scope.actdateorig.date = $scope.acdate.date.toString().substring(0,15);
+			console.log("apply date change: " + JSON.stringify($scope.acdateorig) + " .. " + JSON.stringify($scope.acdate));
+		}
+		$scope.applyDateChangeNew = function(d) {
+			console.log("apply new date to old one");
+			//$scope.setActiveDate(d);
+			$scope.acdate.date = angular.copy(d);
+		}
 		$scope.setActiveTime = function(t) {
+			//$scope.actimeorig = t;
+			$scope.actime = angular.copy(t);
 			$scope.actime = t;
+			
+			$scope.$watch('actime.stamp', function(newvalue,oldvalue) {
+				if((newvalue != $scope.actimeorig.stamp) && (newvalue.match(/^([2][0-3]|[01]?[0-9])([.:][0-5][0-9])?$/)))
+					$scope.showApplyTime(true);
+				else
+					$scope.showApplyTime(false);
+			});
+		}
+		$scope.showApplyTime = function(show){
+			$scope.isCollapsedTime = !show;
+		}
+		$scope.applyTimeChange = function() {
+		
+			console.log("apply time change:" + JSON.stringify($scope.actimeorig) + " -- " + JSON.stringify($scope.actime));
+			$scope.actimeorig.stamp = $scope.actime.stamp;
+			console.log("apply time change:" + JSON.stringify($scope.actimeorig) + " -- " + JSON.stringify($scope.actime));
+
 		}
 		$scope.deleteActiveTime = function(i) {
 			$scope.items.meeting.data.times.splice(i,1);
@@ -82,12 +126,27 @@ angular.module('ngtestApp')
 
 		$scope.toggled = function(open) {
 			console.log("Dropdown is now");
-		};		
-		$scope.isCollapsed = true;
-		$scope.toggleCollapsed = function() {
-			console.log("toggle collapsed");
-			$scope.isCollapsed = !($scope.isCollapsed);
+		};
+		$scope.isCollapsedDate = true;
+		$scope.isCollapsedTime = false;
+		$scope.toggleCollapsedTime = function() {
+			console.log("toggle collapsedTime");
+			$scope.isCollapsedTime = !($scope.isCollapsedTime);
 		}
+		$scope.toggleCollapsedDate = function() {
+			console.log("toggle collapsedDate");
+			$scope.isCollapsedDate = !($scope.isCollapsedDate);
+		}
+		$scope.showAlert = function(ev) {
+			$mdDialog.show(
+			  $mdDialog.alert()
+				.title('This is an alert title')
+				.content('You can specify some description text in here.')
+				.ariaLabel('Password notification')
+				.ok('Got it!')
+				.targetEvent(ev)
+			);
+		}	
 	}   
 
     $scope.loadJSON = function(def) {
@@ -125,5 +184,18 @@ angular.module('ngtestApp')
 	  transclude: true,
       templateUrl: 'views/dk-control.html'
     };
-  }); //ericDlg
+  });
+  /*
+  .directive('watchChange', function() {
+    return {
+        scope: {
+            onchange: '&watchChange'
+        },
+        link: function(scope, element, attrs) {
+            element.on('md-text-float', function() {
+                scope.onchange();
+            });
+        }
+    };
+});*/
 
